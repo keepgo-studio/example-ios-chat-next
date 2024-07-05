380caa3c70c9e0f1e8132aec90dc378d92b2006c
keepgo-studio <keepgo.studio@gmail.com>
2024-07-05 17:48:37 +0900

    Fix docs/

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
+const prefix = process.env.NODE_ENV === 'production' ? 'https://keepgo-studio.github.io/example-ios-chat-next/' : ''
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   output: "export",
   distDir: "docs",
-  basePath: "/example-ios-chat-next"
+  basePath: "/example-ios-chat-next",
+  assetPrefix: prefix
 };
 
 export default nextConfig;
